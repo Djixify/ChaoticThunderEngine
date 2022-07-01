b5fe6bb3dd6148bdc6d30575c531c0304ca24b41
Djixify <tobias.floor@gmail.com>
2022-07-01 09:11:43 +0200

    Removed all files in hopes it will update glfw folder properly

--- ChaoticThunderEngineProject/IDisposable.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef INTERFACE_DISPOSABLE
-#define INTERFACE_DISPOSABLE
-
-class IDisposable {
-public:
-    virtual void Dispose() { throw new std::exception("Interface function not implemented") }
-};
-
-#endif
